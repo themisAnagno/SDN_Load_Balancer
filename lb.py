714f7a7e47a508acf326dde366a8888f191efec3
themis <thmanangostopoulos@gmail.com>
2019-10-18 18:28:27 +0000

    Post users - Start LB

--- lb.py
+++ lb.py
@@ -1,6 +1,48 @@
+import requests
+import time
+import threading
+import logging
+
 import lb_api
 
 
-print("IoRL Load Balancer Application starts")
+# Create the logger
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+stream_handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
 
-lb_api.create_app()
+logger.info("IoRL Load Balancer Application starts")
+
+# Create the flask app for implementing APIs
+api_thread = threading.Thread(target=lb_api.create_app, args=(), daemon=True)
+api_thread.start()
+
+
+def run_lb():
+    """
+    Starts the Load Balancer application
+    """
+
+    # Initialize the traffic variable
+    response = requests.get(url="http://10.100.128.2:8080/stats/port/\
+                                182816038959173/2")
+    traffic = response.json()["182816038959173"][0]["rx_bytes"]
+    while True:
+        response = requests.get(url="http://10.100.128.2:8080/stats/port/\
+                                182816038959173/2")
+        new_traffic = response.json()["182816038959173"][0]["rx_bytes"]
+        # Calculate Mbps
+        bytes_per_sec = (new_traffic - traffic)/10
+        Mbytes_per_sec = bytes_per_sec/1000000
+        bitrate = Mbytes_per_sec * 8
+        if bitrate > 50:
+            logger.debug(f"The bitrate is over the thresehold ({bitrate})")
+        traffic = new_traffic
+        time.sleep(10)
+
+
+if (__name__ == "__main__"):
+    run_lb()
--- lb_api.py
+++ lb_api.py
@@ -1,24 +1,37 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import logging
 
 
-rules = [{
-    'name': 'My Store',
-    'items': [{'name': 'my item', 'price': 15.99}]
-}]
+# Create the logger
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+stream_handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+
+users = []
 
 
-class Rules(Resource):
-	"""
-	The resource that contains the rules that have been applied by the load balancer
-	"""
-	def get(self):
-		return rules, 200
+class Users(Resource):
+    """
+    The resource that contains the rules that have been applied by the load
+    balancer
+    """
+    def get(self):
+        return users, 200
+
+    def post(self):
+        new_users = request.get_json()
+        logger.debug(new_users)
+        users.append(new_users)
+        return "New users added succesfully", 201
 
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
-    
-    api.add_resource(Rules, '/api/rules')
+
+    api.add_resource(Users, '/api/users')
     app.run(host='0.0.0.0', port=8001)
